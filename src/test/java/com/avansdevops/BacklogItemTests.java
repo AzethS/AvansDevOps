e26c551a1f401c09a3fb38ad43cd8e4ad448633b
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2024-06-19 18:38:55 +0200

    Backlog item tests and more discussion tests

---
+++ src/test/java/com/avansdevops/BacklogItemTests.java
@@ -0,0 +1,213 @@
+package com.avansdevops;
+
+import com.avansdevops.notifications.strategy.NotificationStrategy;
+import com.avansdevops.sprint.Sprint;
+import com.avansdevops.sprint.backlog.BacklogItem;
+import com.avansdevops.sprint.backlog.states.BacklogItemStateType;
+import com.avansdevops.user.Role;
+import com.avansdevops.user.User;
+import nl.altindag.log.LogCaptor;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+class BacklogItemTests {
+    private static LogCaptor logCaptor;
+
+    @BeforeAll
+    public static void setupLogCaptor() {
+        logCaptor = LogCaptor.forClass(AvansDevOps.class);
+        logCaptor.disableConsoleOutput();
+    }
+
+    @AfterEach
+    public void clearLogs() {
+        logCaptor.clearLogs();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        logCaptor.close();
+    }
+
+    @Test
+    void backlogItemWithDoneStateShouldBeDone() {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Done");
+        item.setState(BacklogItemStateType.DONE.create(item));
+
+        Assertions.assertTrue(item.isDone());
+    }
+
+    @Test
+    void backlogItemMovedToReadyForTestingShouldNotifyTesters() {
+        MockedBacklogItem mock = createMockedBacklogItemWithState(BacklogItemStateType.DOING, "Test Item 1");
+
+        mock.item.getState().transferToReadyForTesting();
+
+        Mockito.verify(mock.tester, Mockito.times(1)).sendNotification(Mockito.anyString());
+
+        Mockito.verify(mock.leadDeveloper, Mockito.times(0)).sendNotification(Mockito.anyString());
+        Mockito.verify(mock.scrumMaster, Mockito.times(0)).sendNotification(Mockito.anyString());
+    }
+
+    @Test
+    void backlogItemMovedFromTestingToTodoShouldNotifyScrumMaster() {
+        MockedBacklogItem mock = createMockedBacklogItemWithState(BacklogItemStateType.TESTING, "Test Item 2");
+
+        mock.item.getState().transferToTodo();
+
+        Mockito.verify(mock.scrumMaster, Mockito.times(1)).sendNotification(Mockito.anyString());
+
+        Mockito.verify(mock.leadDeveloper, Mockito.times(0)).sendNotification(Mockito.anyString());
+        Mockito.verify(mock.tester, Mockito.times(0)).sendNotification(Mockito.anyString());
+    }
+
+    @Test
+    void backlogItemMovedToTestedShouldNotifyLeadDevelopers() throws NoSuchMethodException {
+        MockedBacklogItem mock = createMockedBacklogItemWithState(BacklogItemStateType.TESTING, "Test Item 3");
+
+        mock.item.getState().transferToTested();
+
+        Mockito.verify(mock.leadDeveloper, Mockito.times(1)).sendNotification(Mockito.anyString());
+
+        Mockito.verify(mock.scrumMaster, Mockito.times(0)).sendNotification(Mockito.anyString());
+        Mockito.verify(mock.tester, Mockito.times(0)).sendNotification(Mockito.anyString());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, true",
+            "transferToDoing, false",
+            "transferToReadyForTesting, true",
+            "transferToTesting, true",
+            "transferToTested, true",
+            "transferToDone, true"
+    })
+    void transferringFromTodoToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Todo");
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, false",
+            "transferToDoing, true",
+            "transferToReadyForTesting, false",
+            "transferToTesting, true",
+            "transferToTested, true",
+            "transferToDone, true"
+    })
+    void transferringFromDoingToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Doing");
+        item.setState(BacklogItemStateType.DOING.create(item));
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, true",
+            "transferToDoing, true",
+            "transferToReadyForTesting, true",
+            "transferToTesting, false",
+            "transferToTested, true",
+            "transferToDone, true"
+    })
+    void transferringFromReadyForTestingToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Ready For Testing");
+        item.setState(BacklogItemStateType.READY_FOR_TESTING.create(item));
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, false",
+            "transferToDoing, true",
+            "transferToReadyForTesting, true",
+            "transferToTesting, true",
+            "transferToTested, false",
+            "transferToDone, true"
+    })
+    void transferringFromTestingToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Testing");
+        item.setState(BacklogItemStateType.TESTING.create(item));
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, true",
+            "transferToDoing, true",
+            "transferToReadyForTesting, false",
+            "transferToTesting, true",
+            "transferToTested, true",
+            "transferToDone, false"
+    })
+    void transferringFromTestedToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Tested");
+        item.setState(BacklogItemStateType.TESTED.create(item));
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "transferToTodo, true",
+            "transferToDoing, true",
+            "transferToReadyForTesting, true",
+            "transferToTesting, true",
+            "transferToTested, true",
+            "transferToDone, true"
+    })
+    void transferringFromDoneToInvalidStateShouldFail(String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        BacklogItem item = new BacklogItem(new Sprint(), "Test Item | Done");
+        item.setState(BacklogItemStateType.DONE.create(item));
+
+        assertThrows(item.getState(), methodName, shouldThrow);
+    }
+
+    private static void assertThrows(Object obj, String methodName, boolean shouldThrow) throws NoSuchMethodException {
+        Method method = obj.getClass().getMethod(methodName);
+        if (shouldThrow) {
+            // Method#invoke throws an InvocationTargetException when the underlying method throws an exception.
+            Assertions.assertThrows(InvocationTargetException.class, () -> method.invoke(obj));
+        } else {
+            Assertions.assertDoesNotThrow(() -> method.invoke(obj));
+        }
+    }
+
+    private static MockedBacklogItem createMockedBacklogItemWithState(BacklogItemStateType stateType, String name) {
+        Sprint sprint = new Sprint();
+
+        NotificationStrategy leadDeveloperStrategy = Mockito.mock(NotificationStrategy.class);
+        NotificationStrategy scrumMasterStrategy = Mockito.mock(NotificationStrategy.class);
+        NotificationStrategy testerStrategy = Mockito.mock(NotificationStrategy.class);
+
+        User leadDeveloper = new User("Lead_developer", Role.LEAD_DEVELOPER, leadDeveloperStrategy);
+        User scrumMaster = new User("Scrum_master", Role.SCRUM_MASTER, scrumMasterStrategy);
+        User tester = new User("Tester", Role.TESTER, testerStrategy);
+
+        sprint.addParticipant(leadDeveloper);
+        sprint.addParticipant(scrumMaster);
+        sprint.addParticipant(tester);
+
+        BacklogItem item = new BacklogItem(sprint, name);
+        item.setState(stateType.create(item));
+        return new MockedBacklogItem(item, leadDeveloperStrategy, scrumMasterStrategy, testerStrategy);
+    }
+
+    private record MockedBacklogItem(
+            BacklogItem item,
+            NotificationStrategy leadDeveloper,
+            NotificationStrategy scrumMaster,
+            NotificationStrategy tester) {
+    }
+}
--- src/test/java/com/avansdevops/DiscussionTests.java
+++ src/test/java/com/avansdevops/DiscussionTests.java
@@ -2,15 +2,35 @@ package com.avansdevops;
 
 import com.avansdevops.discussion.Comment;
 import com.avansdevops.discussion.Discussion;
+import com.avansdevops.discussion.visitor.DiscussionPrintVisitor;
 import com.avansdevops.discussion.visitor.DiscussionVisitor;
 import com.avansdevops.sprint.Sprint;
 import com.avansdevops.sprint.backlog.BacklogItem;
 import com.avansdevops.sprint.backlog.states.BacklogItemStateType;
 import com.avansdevops.user.User;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import nl.altindag.log.LogCaptor;
+import org.junit.jupiter.api.*;
 
-public class DiscussionTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DiscussionTests {
+    private static LogCaptor logCaptor;
+
+    @BeforeAll
+    public static void setupLogCaptor() {
+        logCaptor = LogCaptor.forClass(AvansDevOps.class);
+        logCaptor.disableConsoleOutput();
+    }
+
+    @AfterEach
+    public void clearLogs() {
+        logCaptor.clearLogs();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        logCaptor.close();
+    }
 
     @Test
     void addCommentToFinishedBacklogItemShouldFail() {
@@ -46,7 +66,70 @@ public class DiscussionTests {
     }
 
     @Test
+    void buildCommentWithoutAuthorShouldFail() {
+        Discussion discussion = createEditableDiscussion();
+
+        Assertions.assertThrows(IllegalStateException.class, () -> discussion.comment()
+                .content("This is a simple comment")
+                .build()
+        );
+    }
+
+    @Test
+    void buildCommentWithoutContentShouldFail() {
+        User user = new User("John");
+        Discussion discussion = createEditableDiscussion();
+
+        Assertions.assertThrows(IllegalStateException.class, () -> discussion.comment()
+                .author(user)
+                .build()
+        );
+    }
+
+    @Test
     void commentRepliesShouldBeNestedCorrectly() {
+        Discussion discussion = createEditableDiscussion();
+
+        discussion.accept(new DiscussionVisitor() {
+            @Override
+            public void visit(Discussion discussion) {
+            }
+
+            @Override
+            public void visit(Comment comment) {
+                switch (comment.getContent()) {
+                    case "This is a simple comment",
+                         "This is a comment with replies" -> {
+                        Assertions.assertNull(comment.getParent());
+                    }
+                    case "This is a reply" -> {
+                        Assertions.assertNotNull(comment.getParent());
+                        Assertions.assertEquals("This is a comment with replies", comment.getParent().getContent());
+                    }
+                    case "This is a reply to a reply" -> {
+                        Assertions.assertNotNull(comment.getParent());
+                        Assertions.assertEquals("This is a reply", comment.getParent().getContent());
+                    }
+                }
+            }
+        });
+    }
+
+    @Test
+    void printVisitorOutputShouldBeCorrect() {
+        Discussion discussion = createEditableDiscussion();
+        discussion.accept(new DiscussionPrintVisitor());
+
+        assertThat(logCaptor.getInfoLogs()).containsExactly(
+                "Discussion: Todo Backlog Item | Open",
+                "Comment: Jack -> This is a simple comment",
+                "Comment: John -> This is a comment with replies",
+                "Reply to (John -> This is a comment with replies): Jane -> This is a reply",
+                "Reply to (Jane -> This is a reply): Jack -> This is a reply to a reply"
+        );
+    }
+
+    private static Discussion createEditableDiscussion() {
         User user = new User("John");
         User user2 = new User("Jane");
         User user3 = new User("Jack");
@@ -72,29 +155,6 @@ public class DiscussionTests {
                         )
                 ).build()
         );
-
-        discussion.accept(new DiscussionVisitor() {
-            @Override
-            public void visit(Discussion discussion) {
-            }
-
-            @Override
-            public void visit(Comment comment) {
-                switch (comment.getContent()) {
-                    case "This is a simple comment",
-                         "This is a comment with replies" -> {
-                        Assertions.assertNull(comment.getParent());
-                    }
-                    case "This is a reply" -> {
-                        Assertions.assertNotNull(comment.getParent());
-                        Assertions.assertEquals("This is a comment with replies", comment.getParent().getContent());
-                    }
-                    case "This is a reply to a reply" -> {
-                        Assertions.assertNotNull(comment.getParent());
-                        Assertions.assertEquals("This is a reply", comment.getParent().getContent());
-                    }
-                }
-            }
-        });
+        return discussion;
     }
 }
